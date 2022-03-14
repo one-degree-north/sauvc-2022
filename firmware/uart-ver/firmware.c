c92982b41c19b141ad52d02ff8c6d65e0509f174
lutet <xhex88jeff@gmail.com>
2022-03-14 02:24:27 -0400

    firmware-feat: initial uart interface

--- firmware/uart-ver/firmware.c
+++ firmware/uart-ver/firmware.c
@@ -6,6 +6,15 @@
 #include "hardware/i2c.h"
 #include "hardware/adc.h"
 
+
+// convenience things...
+#define u8              uint8_t
+#define u16             uint16_t
+#define u32             uint32_t
+#define i8              uint8_t
+#define i16             uint16_t
+#define i32             uint32_t
+
 // define GPIO pins
 #define THRUSTER_ONE    7
 #define THRUSTER_TWO    8
@@ -37,57 +46,139 @@
 
 
 // alias for constants
-const uint8_t thrusterPins[] = {THRUSTER_ONE, THRUSTER_TWO, THRUSTER_THREE,
+const u8 thrusterPins[] = {THRUSTER_ONE, THRUSTER_TWO, THRUSTER_THREE,
                               THRUSTER_FOUR, THRUSTER_FIVE, THRUSTER_SIX};
-const uint8_t servoPins[] = {SERVO_LEFT, SERVO_RIGHT};
-const uint8_t killSwitch = KILLSWITCH;
-const uint8_t numThrusters = 6;
-const uint8_t numServos = 2;
+const u8 servoPins[] = {SERVO_LEFT, SERVO_RIGHT};
+const u8 killSwitch = KILLSWITCH;
+const u8 numThrusters = 6;
+const u8 numServos = 2;
 
-// arduino clone lol
-int main() {
-    setup();
+/*** SECTION: UTILITIES ***/
 
-    while(true) {
-        loop();
+bool assertRange(i32 value, i32 min, i32 max) {
+    return min <= value && value <= max;
+}
+
+/*** SECTION: COMMUNICATIONS ***/
+
+#define MAX_UART_QUEUE      45
+#define PACKET_HEADER       0x4d
+#define PACKET_FOOTER       0x5c
+
+u8 queue[MAX_UART_QUEUE];
+u8* queuePtr = queue;
+u8 lrc = 0x00;
+u8 packetLength = 0;
+
+void setupUART() {
+    uart_init(uart0, 230400);
+
+    // UART0 on Feather RP2040 is bound to GPIO0, GPIO1
+    gpio_set_function(0, GPIO_FUNC_UART);
+    gpio_set_function(1, GPIO_FUNC_UART);
+}
+
+void resetQueue() {
+    // too many bytes! reset the queue
+    queuePtr = queue;
+    lrc = 0x00;
+    packetLength = 0;
+}
+
+// read the byte c into queue
+void readByte(u8 c) {
+    *queuePtr = c;
+    queuePtr++;
+    lrc += c;
+    lrc &= 0xFF;
+}
+
+void parsePacket() {
+    // by this point, the packet's structure must be valid.
+    // but make sure header is correct for sanity's sake
+    if (queue[0] != PACKET_HEADER) return;
+
+    u8 cmd = queue[1];
+    u8 param = queue[2];
+    u8 len = queue[3];
+    u8* data = queue + 4;
+
+    // temporary
+    return;
+}
+
+// parse the byte c as part of the packet
+void parseByte(u8 c) {
+    u8 offset = (queuePtr - queue) / sizeof(u8);
+    if (offset >= MAX_UART_QUEUE) {
+        resetQueue();
+        return;
     }
 
-    return 0;
+    if (offset == 3) {
+        // byte 3 defines final packet length, or 6+c
+        if (packetLength > 16) {
+            resetQueue();
+            return;
+        }
+        packetLength = 6 + c;
+        readByte(c);
+        return;
+    }
+
+    if (offset == packetLength - 2) {
+        // byte len-2: longitudinal redundancy check
+        lrc = ((lrc ^ 0xFF) + 1) & 0xFF;
+        if (lrc != c) {
+            resetQueue();
+            return;
+        }
+        return;
+    }
+
+    if (offset == packetLength - 1) {
+        // byte len-1: footer
+        if (c == PACKET_FOOTER) {
+            parsePacket();
+        }
+        resetQueue();
+        return;
+    }
+
+    // default case (all other bytes)
+    readByte(c);
 }
 
-void setup() {
-    // before we do ANYTHING, set the system clock to 96MHz
-    set_sys_clock_khz(96000, false);
-    setupOutputs();
-    initKillSwitch(killSwitch);
+void readUART() {
+    if (uart_is_readable(uart0)) {
+        u8 c = uart_getc(uart0);
 
-}
-
-
-void loop() {
-
+        if (queuePtr == queue) {
+            // no command is currently being written
+            // only start writing command if header is detected
+            if (c == PACKET_HEADER) {
+                *queuePtr = c;
+                queuePtr++;
+                lrc = c;
+            }
+        } else {
+            // commands are currently being written
+            parseByte(c);
+        }
+    }
 }
 
 /*** SECTION: THRUSTERS AND SERVOS ***/
-
-void setupOutputs() {
-    for (int i = 0; i < numThrusters; ++i) {
-        initOneShot125(thrusterPins[i]);
-    }
-    for (int i = 0; i < numServos; ++i) {
-        initServo(servoPins[i]);
-    }
-}
 
 void initOneShot125(uint pin) {
     // initialize the pin
     gpio_init(pin);
     gpio_set_function(pin, GPIO_FUNC_PWM);
-    uint8_t slice = pwm_gpio_to_slice_num(pin);
-    uint8_t channel = pwm_gpio_to_channel(pin);
+    u8 slice = pwm_gpio_to_slice_num(pin);
+    u8 channel = pwm_gpio_to_channel(pin);
 
     // configure PWM
-    pwm_set_phase_correct(slice_one, false);
+    pwm_set_phase_correct(slice, false);
     pwm_config config = pwm_get_default_config();
 
     // OneShot125 will run where 1000-2000 map to 125us-250us
@@ -111,11 +202,11 @@ void initServo(uint pin) {
     // initialize the pin
     gpio_init(pin);
     gpio_set_function(pin, GPIO_FUNC_PWM);
-    uint8_t slice = pwm_gpio_to_slice_num(pin);
-    uint8_t channel = pwm_gpio_to_channel(pin);
+    u8 slice = pwm_gpio_to_slice_num(pin);
+    u8 channel = pwm_gpio_to_channel(pin);
 
     // configure PWM
-    pwm_set_phase_correct(slice_one, false);
+    pwm_set_phase_correct(slice, false);
     pwm_config config = pwm_get_default_config();
 
     // OneShot125 will run where 1000-2000 map to 125us-250us
@@ -129,11 +220,40 @@ void initServo(uint pin) {
     pwm_set_chan_level(slice, channel, 1500);
 }
 
+void setServo(uint servo, uint level) {
+    // level: [1000, 2000]
+    if (!assertRange(level, 1000, 2000)) return;
+
+    u8 slice = pwm_gpio_to_slice_num(servoPins[servo]);
+    u8 channel = pwm_gpio_to_channel(servoPins[servo]);
+
+    pwm_set_chan_level(slice, channel, level);
+}
+
+void setThruster(uint thruster, uint level) {
+    // level: [0, 1000]
+    if (!assertRange(level, 0, 1000)) return;
+
+    u8 slice = pwm_gpio_to_slice_num(thrusterPins[thruster]);
+    u8 channel = pwm_gpio_to_channel(thrusterPins[thruster]);
+
+    pwm_set_chan_level(slice, channel, level + 1000);
+}
+
+void setupOutputs() {
+    for (int i = 0; i < numThrusters; ++i) {
+        initOneShot125(thrusterPins[i]);
+    }
+    for (int i = 0; i < numServos; ++i) {
+        initServo(servoPins[i]);
+    }
+}
+
 /*** SECTION: ANALOG INPUTS ***/
 
 void initKillSwitch(uint pin) {
     gpio_init(pin);
-    gpio_set_dir(pin, GPIO_IN)
+    gpio_set_dir(pin, GPIO_IN);
 
     // disable pull up/down for good measure, only takes 4 cpu cycles
     gpio_disable_pulls(pin);
@@ -163,8 +283,34 @@ float getVoltage() {
     return val * conversionFactor;
 }
 
+/*** SECTION: COMMANDS ***/
 
 
+/*** SECTION: MAIN ***/
+
+void setup() {
+    // before we do ANYTHING, set the system clock to 96MHz
+    set_sys_clock_khz(96000, false);
+    setupOutputs();
+    initKillSwitch(killSwitch);
+    initVoltSensor();
+    setupUART();
+}
+
+
+void loop() {
+    readUART();
+}
+
+int main() {
+    setup();
+
+    while(true) {
+        loop();
+    }
+
+    return 0;
+}
 
 
 
